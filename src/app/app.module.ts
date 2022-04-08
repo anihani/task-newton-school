9108e63f863ebe8581fbb9bf743968d6039c21f4
Anilkumar <anilkumarhj07@gmail.com>
2022-04-08 17:36:19 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
